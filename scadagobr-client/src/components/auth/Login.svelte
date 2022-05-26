2656c6227a0bff6ff3dd8ae1381bae7f6b82c479
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2022-05-26 17:23:57 -0400

    chore: add storybook and ts

---
+++ scadagobr-client/src/components/auth/Login.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let email = "";
+    let password = ""
+</script>
+
+<main>
+    <form class="content">
+
+        <p>Email:</p>
+        <input type="email" required bind:value={email}>
+
+        <p>Password:</p>
+        <input type="password" required bind:value={password}>
+
+        <br>
+        <br>
+
+        <button type="submit">
+            Login
+        </button>
+
+        <button type="submit">
+            Forgot password
+        </button>
+    </form>
+</main>
